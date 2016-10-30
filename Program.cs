2a70228e29dfb1d28152fff8eebb5df306619053
Yang <497091554@qq.com>
2016-10-30 23:34:14 +0800

    2016-10-30-yang

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer = File.ReadAllBytes("log.txt");
+            buffer = Encoding.Convert(Encoding.GetEncoding("GBK"), Encoding.UTF8, buffer);
+            File.WriteAllBytes("a.txt", buffer);
+        }
+    }
+}
